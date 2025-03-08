46dcaa14e9e4bed5f39e341f8439796d5acffa95
Parth Sharma <parth.sharma.nith@gmail.com>
2025-03-08 22:18:30 +0530

    Added first problem and solution

---
+++ solutions/numpy_array_operations.py
@@ -0,0 +1,14 @@
+'''
+👉 Write a Python function that:
+
+1. Creates a NumPy array from a given list of numbers.
+2. Computes the sum, mean, and standard deviation of the array.
+3. Returns these values.
+
+'''
+
+import numpy as np
+
+arr = np.random.rand(7)
+
+print(arr)
